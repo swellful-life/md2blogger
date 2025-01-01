8005f911cddeee3092a8e75bfb59ba9dd5d853e0
swellful <swellful.blog@gmail.com>
2025-01-01 22:09:45 +0900

    Github workflow test. (2025-01-01)

--- src/md2blogger.py
+++ src/md2blogger.py
@@ -1,17 +1,32 @@
 import argparse
 import json
+import os
 
-def filter_md_files(files):
-    return [file for file in files if file.endswith('.md')]
+
+def filter_and_transform_files(files):
+    filtered_files = []
+    for file in files:
+        if file.endswith('.md'):
+            transformed_file = file.replace(' ', '_')
+            filtered_files.append(transformed_file)
+    return filtered_files
+
 
 def main():
-    parser = argparse.ArgumentParser(description="Filter and process .md files.")
+    parser = argparse.ArgumentParser(description="Filter and transform .md files.")
     parser.add_argument('--files', type=str, required=True, help="JSON array of changed files")
     args = parser.parse_args()
 
-    changed_files = json.loads(args.files)
-    md_files = filter_md_files(changed_files)
-    print(json.dumps(md_files, indent=2))
+    try:
+        changed_files = json.loads(args.files)
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return
+
+    md_files = filter_and_transform_files(changed_files)
+
+    print(json.dumps(md_files, ensure_ascii=False, indent=2))
+
 
 if __name__ == "__main__":
     main()
