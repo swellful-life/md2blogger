dec8e8d4a025f1d56d02a6090cbc0a6219c067af
swellful <swellful.blog@gmail.com>
2025-01-11 17:49:46 +0900

    utils/markdown_to_html.py 구현. (2025-01-11)

--- src/utils/markdown_to_html.py
+++ src/utils/markdown_to_html.py
@@ -9,16 +9,17 @@ from zoneinfo import ZoneInfo
 
 @dataclass
 class MetaData:
-    title: Optional[str] = None
-    permalink: Optional[str] = None
-    published: Optional[str] = None
-    tags: Optional[List[str]] = field(default=None)
+    title: str
+    permalink: str
+    published: str
+    tags: List[str] = field(default_factory=list)
 
 class MdToHtml:
     PERMALINK_PATTERN = r"^[a-z\-]+$"
 
-    def __init__(self, input_path: str):
+    def __init__(self, input_path: str, repo_url: str):
         self._input_path = input_path
+        self._repo_url = repo_url
         self._contents = self._read_file()
         self._properties, self._body = self._parse_contents(self._contents)
 
@@ -52,35 +53,56 @@ class MdToHtml:
             return False
 
     @staticmethod
-    def _parse_datetime(date_str: str) -> str:
+    def _parse_datetime(date_utc: Optional[str]) -> str:
         try:
-            utc_datetime = datetime.fromisoformat(date_str)
-            kst_datetime = utc_datetime.replace(tzinfo=ZoneInfo("UTC")).astimezone(ZoneInfo("Asia/Seoul"))
+            if not date_utc:
+                return datetime.now(ZoneInfo("Asia/Seoul")).isoformat(timespec='seconds')
+
+            date_utc_obj = datetime.fromisoformat(date_utc)
+            kst_datetime = date_utc_obj.replace(tzinfo=ZoneInfo("UTC")).astimezone(ZoneInfo("Asia/Seoul"))
             return kst_datetime.isoformat(timespec='seconds')
-        except ValueError:
+        except (ValueError, TypeError):
             return datetime.now(ZoneInfo("Asia/Seoul")).isoformat(timespec='seconds')
 
-    def _validate_permalink(self, permalink: str) -> str:
+    def _validate_permalink(self, permalink: Optional[str]) -> Optional[str]:
+        if not permalink:
+            return None
         permalink = permalink.replace(" ", "")
-        print(permalink)
-        return permalink if re.fullmatch(self.PERMALINK_PATTERN, permalink) else ""
+        return permalink if re.fullmatch(self.PERMALINK_PATTERN, permalink) else None
 
-    def extract_metadata(self) -> Optional[MetaData]:
-        if not self._properties:
-            return MetaData(
-            title=os.path.basename(self._input_path).rsplit(".md", 1)[0],
-            published = self._parse_datetime(""),
-        )
+    def extract_metadata(self) -> MetaData:
+        default_title = os.path.basename(self._input_path).rsplit(".md", 1)[0]
 
         return MetaData(
-            title=self._properties.get("title", os.path.basename(self._input_path).rsplit(".md", 1)[0]),
-            permalink=self._validate_permalink(self._properties.get("permalink", None)),
-            published=self._parse_datetime(self._properties.get("published", "")),
+            title=self._properties.get("title", default_title),
+            permalink=self._validate_permalink(self._properties.get("permalink")),
+            published=self._parse_datetime(self._properties.get("published")),
             tags=list(self._properties.get("tags", [])),
         )
 
-    def _md_to_html(self):
+    def _convert_md_to_html(self):
+        from markdown_it import MarkdownIt
+        from mdit_py_plugins.footnote import footnote_plugin
+        from mdit_py_plugins.tasklists import tasklists_plugin
+
         self._metadata = self.extract_metadata()
+
+        md_renderer = (
+            MarkdownIt("commonmark")
+            .use(tasklists_plugin)
+            .enable("table")
+        )
+
+        html = md_renderer.render(self._body)
+
+        html_with_repo_url = re.sub(
+            r'(<img\s+[^>]*src=")(?!http)([^"]+)(")',  # Match <img src="...">
+            rf'\1{self._repo_url}/blob/main/\2?raw=true\3 width="100%"',  # Add width="100%"
+            html,
+        )
+        print(f"\nmetadata: {self._metadata}")
+        print(f"body: \n{html_with_repo_url}")
+
         # TODO: Convert markdown in self._body to HTML.
         # TODO: If the title is not set in metadata, derive it from the file name.
         # TODO: Return or store the resulting HTML content.
